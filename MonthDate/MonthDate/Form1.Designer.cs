bda7a3ff4ef210fa8119ecc3d309454b33ce84bc
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-01-18 13:07:41 +0100

    Add files via upload

---
+++ MonthDate/MonthDate/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace MonthDate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monCal = new System.Windows.Forms.MonthCalendar();
+            this.btnOdabir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monCal
+            // 
+            this.monCal.Location = new System.Drawing.Point(18, 18);
+            this.monCal.MaxSelectionCount = 14;
+            this.monCal.Name = "monCal";
+            this.monCal.TabIndex = 0;
+            // 
+            // btnOdabir
+            // 
+            this.btnOdabir.Location = new System.Drawing.Point(18, 192);
+            this.btnOdabir.Name = "btnOdabir";
+            this.btnOdabir.Size = new System.Drawing.Size(199, 72);
+            this.btnOdabir.TabIndex = 1;
+            this.btnOdabir.Text = "Odabir datuma";
+            this.btnOdabir.UseVisualStyleBackColor = true;
+            this.btnOdabir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 398);
+            this.Controls.Add(this.btnOdabir);
+            this.Controls.Add(this.monCal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monCal;
+        private System.Windows.Forms.Button btnOdabir;
+    }
+}
+
---
+++ MonthDate/MonthDate/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonthDate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        TimeSpan period = monCal.SelectionEnd
+                - monCal.SelectionStart;
+
+
+            if (period.Days < 1)
+            {
+                MessageBox.Show("Niste odabrali datum ",
+                "GREŠKA",
+              MessageBoxButtons.OK,
+              MessageBoxIcon.Information) ;
+            }
+            else
+            {
+                MessageBox.Show("Rezervirali ste "
+                    + monCal.SelectionStart.ToShortDateString
+                    + " od "
+                    + monCal.SelectionEnd.ToShortDateString
+                    + ".",
+                    "rezervacija",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+    }
+}
